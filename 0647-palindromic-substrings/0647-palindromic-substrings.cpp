68197af893e032d7efd03ec152ad03ec9301632c
Rudra <125184056+rudraranjan001@users.noreply.github.com>
2026-08-28 20:09:33 +0530

    Time: 33 ms (27.83%), Space: 13 MB (34.68%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-    bool palindrome(string s){
-        string str = s;
-        reverse(str.begin(),str.end());
-        return s == str;
+    int t[1001][1001];
+    bool palindrome(string &s, int i, int j){
+        if(i > j) return  true;
+
+        if(t[i][j] != -1) return t[i][j];
+
+        if(s[i] == s[j])    return t[i][j] = palindrome(s,i+1,j-1);
+
+        return false;
     }
     int countSubstrings(string s) {
+        memset(t,-1,sizeof(t));
         int n = s.size();
         int c = 0;
-        for(int i = n-1; i >= 0; i--){
+        for(int i = 0; i < n; i++){
             for(int j = i; j < n; j++){
-                if(palindrome(s.substr(i,j-i+1)))  c++;
+                if(palindrome(s,i,j))  c++;
             }
         }
         return c;
