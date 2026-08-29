d63d4ed7938e08c00676cc7e3ae0c580fbf86984
Rudra <125184056+rudraranjan001@users.noreply.github.com>
2026-08-30 03:15:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Marks from Ranks/marks-from-ranks.cpp
@@ -0,0 +1,46 @@
+class Solution {
+  public:
+    int findInterval(vector<int> &prefix, int low, int high, int rank)
+    {
+        while (low < high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if (prefix[mid] < rank)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+
+        return low;
+    }
+    vector<int> getMarks(vector<int> &l, vector<int> &r, vector<int> &rank) {
+        // code here
+        int n = l.size();
+
+        // Stores the cumulative number of marks till each interval
+        vector<int> prefix(n);
+
+        prefix[0] = r[0] - l[0] + 1;
+
+        for (int i = 1; i < n; i++)
+        {
+            prefix[i] = prefix[i - 1] + (r[i] - l[i] + 1);
+        }
+
+        vector<int> ans(rank.size());
+
+        // Process every query
+        for (int i = 0; i < rank.size(); i++)
+        {
+            // Find the interval containing the required rank
+            int idx = findInterval(prefix, 0, n - 1, rank[i]);
+
+            // Compute the corresponding mark
+            int diff = prefix[idx] - rank[i];
+            ans[i] = r[idx] - diff;
+        }
+
+        return ans;
+    }
+};
